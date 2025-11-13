b0d04d62c9c67e95bbd872d9bd835fa1d2a20123
Youssef Mahmoud <kamel.youssef1@gmail.com>
2025-11-12 22:02:43 -0800

    Make an API endpoint for creating posts
    - Created migrations for missing tags and updated_at in posts table
    - Created posts API endpoint

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,7 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("ok"))
+	data := map[string]string{
+		"status":  "ok",
+		"env":     app.config.env,
+		"version": version,
+	}
+	if err := writeJSON(w, http.StatusOK, data); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+	}
 }

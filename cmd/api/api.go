52c0e698c586e45cef06bdfa442fad66dfcb1b40
Youssef Mahmoud <kamel.youssef1@gmail.com>
2025-11-16 20:26:53 -0800

    Implement endpoints
    - Implement error infrastructure
    - Implement get post endpoint
    - Implement validation of payloads

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -39,6 +39,10 @@ func (app *application) mount() http.Handler {
 
 		r.Route("/posts", func(r chi.Router) {
 			r.Post("/", app.createPostHandler)
+
+			r.Route("/{postID}", func(r chi.Router) {
+				r.Get("/", app.getPostHandler)
+			})
 		})
 	})
 	return r
